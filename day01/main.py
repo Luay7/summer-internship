f9ca7eeae312018536fa6afee33e15b481916e38
Luay <mrcsprogram@gmail.com>
2026-07-06 13:37:13 +0300

    Day 2: Complete prompt engineering task and compare outputs for best results

--- day01/main.py
+++ day01/main.py
@@ -6,31 +6,41 @@ from google import genai
 load_dotenv()
 client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
 
-prompt_1 = "Explain Saudi Arabia in 10 words."
-prompt_2 = "What is the capital ?"
+prompt_1="Explain prompt engineering. Write your answer in approximately 120 words"
+prompt_2="Explain prompt engineering in detail, including its steps and the requirements needed to write an excellent prompt. Write your answer in approximately 120 words"
+prompt_3="I am a student currently doing a summer internship in Artificial Intelligence. Act as an expert instructor and explain prompt engineering to me in an organized and understandable way. Clarify its requirements, the steps to follow, and how to get the best possible output from a prompt. Write your answer in approximately 120 words"
+prompt_4="Act as an experienced Prompt Engineer. Explain prompt engineering in a creative, organized, and sequential manner, using clear examples to illustrate your points. Write your answer in approximately 120 words"
+prompt_5="Role: Expert Prompt Engineer. Task: Explain prompt engineering in an organized and sequential manner with clear examples. Constraints:\nKeep the entire response strictly under 120 words.\nUse bullet points for readability.\nStart directly with the explanation without any introductory or greeting filler."
 
 # Part 2: No Context (Stateless)
 print("--- TEST 1: NO CONTEXT ---")
 
 response_1_stateless = client.models.generate_content(
-    model='gemini-2.5-flash-lite', 
+    model='gemini-2.5-flash', 
     contents=prompt_1
 )
 print(f"Q1: {response_1_stateless.text.strip()}")
 
 response_2_stateless = client.models.generate_content(
-    model='gemini-2.5-flash-lite', 
+    model='gemini-2.5-flash', 
     contents=prompt_2
 )
 print(f"Q2: {response_2_stateless.text.strip()}\n")
 
-# Part 3: With Context (Chat Session)
-print("--- TEST 2: WITH CONTEXT ---")
+response_3_stateless = client.models.generate_content(
+    model='gemini-2.5-flash', 
+    contents=prompt_3
+)
+print(f"Q3: {response_3_stateless.text.strip()}\n")
 
-chat = client.chats.create(model="gemini-2.5-flash-lite")
+response_4_stateless = client.models.generate_content(
+    model='gemini-2.5-flash', 
+    contents=prompt_4
+)
+print(f"Q4: {response_4_stateless.text.strip()}\n")
 
-response_1_stateful = chat.send_message(prompt_1)
-print(f"Q1: {response_1_stateful.text.strip()}")
-
-response_2_stateful = chat.send_message(prompt_2)
-print(f"Q2: {response_2_stateful.text.strip()}")
+response_5_stateless = client.models.generate_content(
+    model='gemini-2.5-flash', 
+    contents=prompt_5
+)
+print(f"Q5: {response_5_stateless.text.strip()}\n")
